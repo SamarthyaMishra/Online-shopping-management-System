ffa8b0432865fde53bb54f6588daa3dfca8311ea
Samarthya Mishra <samarthyamishra01@gmail.com>
2024-05-03 16:19:03 +0530

    first commit

---
+++ src/onlineshoping/UTIL/treemapcl.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+class treemapcl{
+  public static void main(String args[])
+  {
+    TreeMap tm=new TreeMap();
+    tm.put("Ravi",new Double(345.35));
+    tm.put("Raju",new Double(12.45));
+    tm.put("Ram",new Double(90.25));
+    
+    Set s=tm.entrySet();
+    Iterator i=set.iterator();
+ 
+   while(i.hasNext()){
+     Map.Entry m=(Map.Entry)i.next();
+     System.out.print(m.getKey()+" ");
+     System.out.println(m.getValue());
+   }
+   System.out.println();
+   double d=((Double)tm.get("Ravi")).doubleValue();
+   tm.put("Ravi",new Double(d+100));
+   System.out.println("new value of Ravi:"+tm.get("Ravi"));
+ }
+}
+
+  
+    
